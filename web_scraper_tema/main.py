85a1c4dfb1735edeae55147737c77f461420d61c
Bogdan M <bogdancons99@gmail.com>
2020-12-09 20:48:53 +0200

    .

--- web_scraper_tema/main.py
+++ web_scraper_tema/main.py
@@ -6,12 +6,12 @@ URL = 'http://frsah.ro/'
 page = requests.get(url=URL)
 page_content_html = BeautifulSoup(page.content, features='html.parser')
 
-lista = page_content_html.find('div', {'class' : 'td-ss-main-content td_block_template_1'})
+lista = page_content_html.find('div', {'class': 'td-ss-main-content td_block_template_1'})
 lista_title_html = lista.find_all('h3', {'class': 'entry-title td-module-title'})
 
 title_list = []
 for i in lista_title_html:
-    paragraf =i.find('a')
+    paragraf = i.find('a')
     print(paragraf)
     title = paragraf.get('title') or ''
     print(title)
@@ -19,5 +19,4 @@ for i in lista_title_html:
 
 with open('output.txt', 'w', encoding='utf-8') as file:
     for elem in title_list:
-        file.write(elem+'\n')
-
+        file.write(elem + '\n')
